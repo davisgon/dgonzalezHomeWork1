17599253d9786cad1daf922ea850ba8c9fe05858
davisgon <davisgon@gmail.com>
2021-11-18 21:52:55 -0500

    Update HomeWork1.js

--- HomeWork1.js
+++ HomeWork1.js
@@ -26,19 +26,17 @@ console.log(" for (var i = 0; i < 5; i++) { setTimeout(function () { console.log
 console.log("");
 console.log("can use  a closure around setTimeout");
  
-//can use  a closure around setTimeout
-
-for (var i = 0; i < 5; i++) {
-    ((i) => {
-        setTimeout(function () { console.log(i); }, 100);
-    })(i);
-}
-
-
+for (let i = 0; i < 5; i++){
+    setTimeout(function(){
+        console.log(i);
+    },100);
+   }
+//can use  IIFE around setTimeout
 console.log('ES5')
-for (var i = 0; i < 5; i++) {
-    console.log(i);
-    setTimeout(function () { }, 100);
+for (var i = 0; i < 5; i++){
+    (function (i) { 
+        setTimeout(function() {  console.log(i); },100);
+    })(i);
 }
 
 
